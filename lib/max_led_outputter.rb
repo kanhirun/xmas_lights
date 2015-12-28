6f01afe18d1265c9d2595f852800983ddb6db94a
Kel Kanhirun <kel.kanhirun@gmail.com>
2015-12-27 18:06:16 -0600

    refactor: replaces expressions as new methods

--- lib/max_led_outputter.rb
+++ lib/max_led_outputter.rb
@@ -1,19 +1,31 @@
+require 'active_support/core_ext/enumerable'
+
 class MaxLEDOutputter
   def self.puts(input)
     return '' if input == ''
 
-    hours = input.split("\n").map(&:to_i)
-    if hours.length > 1
-      hours.map do |hour|
-        calculator.calculate(hour)
-      end.join("\n") + "\n"
-    else
-      calculator.calculate(hours[0]).to_s
+    data    = data(input)
+    results = data.map do |d|
+      calculator.calculate(d)
     end
+
+    lines(results)
   end
 
   private
     def self.calculator
       MaxLEDCalculator
+    end
+
+    def self.data(lines)
+      lines.split("\n").map(&:to_i)
+    end
+
+    def self.lines(data)
+      if data.many?
+        data.join("\n") + "\n"
+      else
+        data.first.to_s
+      end
     end
 end
