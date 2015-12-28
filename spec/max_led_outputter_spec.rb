babd4f7c42322df37bd58c7cdcdae16747afce08
Kel Kanhirun <kel.kanhirun@gmail.com>
2015-12-27 19:03:10 -0600

    tests: stubs out collaborator for outputter class

--- spec/max_led_outputter_spec.rb
+++ spec/max_led_outputter_spec.rb
@@ -1,10 +1,14 @@
 require 'active_support/core_ext/string'
 require_relative '../lib/max_led_outputter'
 
-# Collaborator
 require_relative '../lib/max_led_calculator'
 
 describe MaxLEDOutputter do
+  let(:calculator) { class_double(MaxLEDCalculator) }
+  before do
+    allow(MaxLEDOutputter).to receive(:calculator) { calculator }
+  end
+
   describe '#puts' do
     it 'returns an empty string when given an empty string' do
       input = ''
@@ -13,12 +17,16 @@ describe MaxLEDOutputter do
     end
 
     it 'returns one line when given one line' do
+      expect(calculator).to receive(:calculate).and_return('300')
       input = '1'
+
       output = MaxLEDOutputter.puts(input)
+
       expect(output).to eq '300'
     end
 
     it 'returns n lines when given n lines' do
+      expect(calculator).to receive(:calculate).and_return('300', '75', '35', '25')
       input = <<-INPUT.strip_heredoc
         1
         4
